7dc79d0cd6219c84aee71d1d86d399bc053c8f91
corystein <corystein@gmail.com>
2016-12-15 14:11:51 -0500

    Added compileScript

--- extension.js
+++ extension.js
@@ -8,12 +8,28 @@ var launch = require('child_process').execFile;
 function activate(context) {
 
     var aiPath = "C:\\Program Files (x86)\\AutoIt3\\AutoIt3.exe"
+    var aiwPath = "C:\\Program Files (x86)\\AutoIt3\\SciTE\\AutoIt3Wrapper\\AutoIt3Wrapper.exe"
     var helpPath = "C:\\Program Files (x86)\\AutoIt3\\AutoIt3Help.exe"
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('AutoIt is now active!');
 
     var runScript = vscode.commands.registerCommand('extension.runScript', function () {
+        
+        //Save the file
+        vscode.window.activeTextEditor.document.save();
+        //Get the current file name
+        var thisFile = vscode.window.activeTextEditor.document.fileName;
+        //Launch the AutoIt executable with the script's path
+        launch(aiPath, [thisFile], (err, stdout, stderr) => {
+            if (err) {
+                vscode.window.showErrorMessage(err);
+                return;
+            }
+        });
+    });
+    
+    var compileScript = vscode.commands.registerCommand('extension.compileScript', function () {
         
         //Save the file
         vscode.window.activeTextEditor.document.save();
